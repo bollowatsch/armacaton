bda4407118ea8bee506030bc8e204a9f4a60839e
Franziska <146085931+Franziska-09@users.noreply.github.com>
2026-04-10 14:10:19 +0200

    create game_manager

--- scripts/mouse.gd
+++ scripts/mouse.gd
@@ -5,6 +5,8 @@ extends Area2D
 
 # Rastergröße: Spieler springt immer um genau 1 Tile
 const TILE_SIZE = 8
+const X_AXIS = 1280
+const Y_AXIS = 920
 
 # Verhindert mehrfaches Drücken pro Frame
 var can_move = true
@@ -29,6 +31,7 @@ func _unhandled_input(event):
 	
 	# SwitchManager gibt die "echte" Richtung zurück
 	# (nach Input-Remapping!)
+	print(event)
 	var input_vector = SwitchManager.input_event_to_vector2(event)
 	var direction = SwitchManager.get_mapped_direction(input_vector)
 	
@@ -39,9 +42,9 @@ func move(dir: Vector2):
 	var target = position + dir * TILE_SIZE
 	
 	# Spielfeldgrenzen prüfen (Spielfeld: 10x8 Tiles)
-	if target.x < 0 or target.x > 9 * TILE_SIZE:
+	if target.x < 0 or target.x > X_AXIS - TILE_SIZE:
 		return
-	if target.y < 0 or target.y > 7 * TILE_SIZE:
+	if target.y < 0 or target.y > Y_AXIS - TILE_SIZE:
 		return
 	
 	can_move = false
