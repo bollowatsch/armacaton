c6a87978b49f54a7a4e85982b5fe735487f59e64
Stephan Bollauf <stephan.bollauf@gmail.com>
2026-04-10 17:39:43 +0200

    main menu linking to first level

--- scenes/ui/button.gd
+++ scenes/ui/button.gd
@@ -1,8 +1,12 @@
 extends Button
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/1.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
