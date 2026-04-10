bfee965324b3716b0f71bb80d51f05c59509a9bb
Stephan Bollauf <stephan.bollauf@gmail.com>
2026-04-10 12:19:24 +0200

    switch coordinates functionality

---
+++ scripts/mouse.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+# Rastergröße: Spieler springt immer um genau 1 Tile
+const TILE_SIZE = 64
+
+# Verhindert mehrfaches Drücken pro Frame
+var can_move = true
+
+func _unhandled_input(event):
+	if not can_move:
+		return
+	
+	# SwitchManager gibt die "echte" Richtung zurück
+	# (nach Input-Remapping!)
+	var direction = SwitchManager.get_mapped_direction(event)
+	
+	if direction != Vector2.ZERO:
+		move(direction)
+
+func move(dir: Vector2):
+	var target = position + dir * TILE_SIZE
+	
+	# Spielfeldgrenzen prüfen (Spielfeld: 10x8 Tiles)
+	if target.x < 0 or target.x > 9 * TILE_SIZE:
+		return
+	if target.y < 0 or target.y > 7 * TILE_SIZE:
+		return
+	
+	can_move = false
+	position = target
+	
+	# Kurze Pause damit man nicht zu schnell drücken kann
+	await get_tree().create_timer(0.1).timeout
+	can_move = true
+	
+	# Ziel erreicht?
+	if position.y == 0:
+		pass
+		#GameManager.win() todo
