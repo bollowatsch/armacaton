fd2dc0bc0f88e191747b6ddd98a5146b80135dd4
Stephan Bollauf <stephan.bollauf@gmail.com>
2026-04-10 19:48:17 +0200

    minor fixes

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,7 +6,7 @@ var LEVEL_START: int = 1
 var LEVELS_AVAILABLE: int = 2
 
 enum State { MENU, PLAYING, DEAD, WIN }
-var state: State = State.PLAYING
+var state: State
 
 var lives: int
 var level: int
@@ -17,6 +17,9 @@ signal lives_changed(new_lives)
 signal level_changed(new_level)
 signal game_over()
 signal game_won()
+
+func _ready() -> void:
+	state = State.MENU
 
 func start_game():
 	lives = LIVES_START
@@ -31,21 +34,14 @@ func player_died():
 	
 	lives -= 1
 	emit_signal("lives_changed", lives)
-	print(lives)
-
 	
 	if lives <= 0:
 		state = State.DEAD
 		mouse.play_dead()
-		print("state dead")
-
-		# TODO back to start screen
-		emit_signal("game_over")
 	else:
 		# Spieler respawnen — Signal an mouse.gd
 		mouse.respawn()
-		#get_tree().call_group("mouse", "respawn")
-
+	
 func player_reached_goal():
 	if state != State.PLAYING:
 		return
@@ -55,15 +51,12 @@ func player_reached_goal():
 	
 	if level > LEVELS_AVAILABLE:
 		state = State.WIN
-		print("state win")
-		# TODO back to start screen
+		go_to_menu()
 	else:
 		get_tree().change_scene_to_file("res://scenes/levels/%d.tscn" % level)
 		mouse.respawn()
-		
 
 func go_to_menu():
-	state = State.MENU
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func restart():
@@ -76,4 +69,3 @@ func register_mouse(m : Mouse1):
 func add_life():
 	lives += 1
 	emit_signal("lives_changed", lives)
-	
