b53444e0a1abab2da742b77bfd2e6f59ce221dcf
Franziska <146085931+Franziska-09@users.noreply.github.com>
2026-04-11 10:10:39 +0200

    implementation of cat direction changes

--- scripts/levels/level2.gd
+++ scripts/levels/level2.gd
@@ -3,3 +3,5 @@ extends Node
 func _ready():
 	LaneManager.set_speed_for_lane($Lanes/Lane2, 200.0)
 	LaneManager.set_speed_for_lane($Lanes/Lane4, 60.0)
+	
+	#LaneManager.set_direction_for_lane($Lanes/Lane2, Vector2.LEFT)
