d6d25d3b0f38f003f4f7ebc873f13b22447d1158
Franziska <146085931+Franziska-09@users.noreply.github.com>
2026-04-11 09:04:34 +0200

    minor changes for scene 2

--- scripts/levels/level1.gd
+++ scripts/levels/level1.gd
@@ -1,8 +1,8 @@
 extends Node
 
 func _ready():
-	LaneManager.set_speed_for_lane($Lanes/Lane3, 150.0)
-	LaneManager.set_speed_for_lane($Lanes/Lane2, 80.0)
+	LaneManager.set_speed_for_lane($Lanes/Lane4, 40.0)
+	LaneManager.set_speed_for_lane($Lanes/Lane5, 110.0)
 
 # Beim Switch-Event:
 func on_switch_triggered():
--- scripts/levels/level2.gd
+++ scripts/levels/level2.gd
@@ -2,4 +2,4 @@ extends Node
 
 func _ready():
 	LaneManager.set_speed_for_lane($Lanes/Lane2, 200.0)
-	
+	LaneManager.set_speed_for_lane($Lanes/Lane4, 60.0)
