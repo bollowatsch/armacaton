e2e2e265cc3695d3fe586e06563f7c729c9b4508
Stephan Bollauf <stephan.bollauf@gmail.com>
2026-04-11 12:38:36 +0200

    add coins to HUD

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 @onready var switch_timer: ProgressBar = $HBoxContainer/SwitchTimer
 @onready var score_label: Label = $HBoxContainer/ScoreLabel
 @onready var timer_label: Label = $HBoxContainer/SwitchTimer/TimerLabel
+@onready var coin_label: Label = $HBoxContainer/CoinLabel
 
 func _ready():
 	add_to_group("hud")
@@ -12,10 +13,12 @@ func _ready():
 	GameManager.lives_changed.connect(update_lives)
 	GameManager.level_changed.connect(update_level)
 	GameManager.score_changed.connect(update_score)
-
+	GameManager.coins_changed.connect(update_coins)
 	
 	update_lives(GameManager.lives)
 	update_level(GameManager.level)
+	update_score(GameManager.score)
+	update_coins(GameManager.coins)
 
 func update_level(new_level: int):
 	level_label.text = "Level: %d" % new_level
@@ -35,3 +38,6 @@ func update_timer(time_remaining: float):
 	
 func update_score(new_score: int):
 	score_label.text = "Score: %d" % new_score
+
+func update_coins(new_coins: int):
+	coin_label.text = "Coins: %d" % new_coins
