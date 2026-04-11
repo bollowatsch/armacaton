b6741e11604ebfc18c3e5d17ff96d8725165e0d1
orthn <nico.orth08@gmail.com>
2026-04-11 15:38:22 +0200

    moved audio files

--- scripts/audio/meowAudio.gd
+++ scripts/audio/meowAudio.gd
@@ -1,8 +1,8 @@
 extends AudioStreamPlayer2D
+# MEOW AUDIO PLAYER
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play()
 	pass # Replace with function body.
 
 
